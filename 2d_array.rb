b9cb94fdef741d8bbe7153f73c4ffbefb1244292
ThundaHorse <abeeeeekim@gmail.com>
2019-05-03 22:58:04 -0500

    removed stray ; symbols from JS conversion

--- 2d_array.rb
+++ 2d_array.rb
@@ -1,11 +1,15 @@
 # Write a function `zip_array(arr1, arr2)` that takes in two arrays and "zips" them together by returning a single 2D-array. Assume that both input arrays have the same length.
 
+
+def zip_array(arr_1, arr_2) 
+
+
+end 
+
 # Examples:
 
 array_1 = ['a', 'b', 'c', 'd']
 array_2 = [10, 20, 30, 40]
-
-
 
 result = zip_array(array_1, array_2)
 p result == [ [ 'a', 10 ], [ 'b', 20 ], [ 'c', 30 ], [ 'd', 40 ] ]
