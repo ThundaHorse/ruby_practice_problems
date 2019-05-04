169a26b86a05252e0ad686f608f84ed2169a38fc
Abraham Kim <49882085+ThundaHorse@users.noreply.github.com>
2019-05-03 23:39:55 -0500

    Update abbreviate.rb

--- abbreviate.rb
+++ abbreviate.rb
@@ -1,10 +1,8 @@
 # Write a function `abbreviate(sentence)` that takes in a sentence string and returns a new sentence where words longer than 4 characters have their vowels removed. Assume the sentence has all lowercase characters.
 # Feel free to use the array below in your solution:
 
-vowels = ['a', 'e', 'i', 'o', 'u'];
-
 def abbreviate(word) 
-  
+  vowels = ['a', 'e', 'i', 'o', 'u']
 
 end 
 
