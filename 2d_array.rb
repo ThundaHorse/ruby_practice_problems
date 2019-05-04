8784bff319199d4280d5b81475a8b49b8222569e
ThundaHorse <abeeeeekim@gmail.com>
2019-05-03 22:57:11 -0500

    removed stray ; symbols from JS conversion

--- 2d_array.rb
+++ 2d_array.rb
@@ -2,15 +2,11 @@
 
 # Examples:
 
-array_1 = ['a', 'b', 'c', 'd'];
-array_2 = [10, 20, 30, 40];
-
-def zip_array(arr_1, arr_2) 
-  
-end 
+array_1 = ['a', 'b', 'c', 'd']
+array_2 = [10, 20, 30, 40]
 
 
 
-result = zip_array(array_1, array_2);
+result = zip_array(array_1, array_2)
 p result == [ [ 'a', 10 ], [ 'b', 20 ], [ 'c', 30 ], [ 'd', 40 ] ]
 
